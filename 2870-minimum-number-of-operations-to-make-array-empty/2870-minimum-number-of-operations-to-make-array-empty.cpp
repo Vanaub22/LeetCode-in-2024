4ee4bd01b11cda188cf28f5913f16f72d5d48a3e
Anuvab Chakravarty <106730134+Vanaub22@users.noreply.github.com>
2024-01-04 12:13:34 +0530

    Time: 110 ms (88.03%), Space: 85.2 MB (46.04%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
+    // Greedy Approach
     int minOperations(vector<int>& nums) {
         unordered_map<int,int> freq;
         for(int i:nums) freq[i]++;
         int step=0;
         for(auto it:freq) {
+            // Try to reduce by 3 as far as possible
             if(it.second%3==0) step+=it.second/3;
             else {
                 int cnt=it.second;
                 while(cnt>0) {
                     if(cnt>=3 && ((cnt-3)%3==0 || (cnt-3)%2==0)) cnt-=3;
                     else if(cnt>=2 && ((cnt-2)%3==0 || (cnt-2)%2==0)) cnt-=2;
-                    else return -1;
+                    else return -1; // Lesser than 2 remaining => cannot be reduced further
                     step++;
                 }
             }
