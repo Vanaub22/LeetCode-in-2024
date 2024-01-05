b2786339f5be4b02108e0836fde45807f0f95798
Anuvab Chakravarty <106730134+Vanaub22@users.noreply.github.com>
2024-01-05 13:51:39 +0530

    Time: 176 ms (82.39%), Space: 121 MB (77.99%) - LeetHub

--- 1357-apply-discount-every-n-orders/1357-apply-discount-every-n-orders.cpp
+++ 1357-apply-discount-every-n-orders/1357-apply-discount-every-n-orders.cpp
@@ -1,13 +1,13 @@
 class Cashier {
 public:
     int n,discount,i;
-    unordered_map<int,int> priceMap; // Using map for quicker access time
+    unordered_map<int,int> priceMap; // Using map for quicker access times
     Cashier(int n, int discount, vector<int>& products, vector<int>& prices) {
         this->i=0;
         this->n=n;
         this->discount=discount;
-        int L=products.size();
-        for(int i=0;i<L;i++) this->priceMap[products[i]]=prices[i];
+        int cnt=products.size();
+        for(int i=0;i<cnt;i++) this->priceMap[products[i]]=prices[i];
     }
     double getBill(vector<int> product, vector<int> amount) {
         this->i++;
