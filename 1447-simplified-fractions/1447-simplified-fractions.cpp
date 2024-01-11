75cf584620d3169d79f7db27de1c4746c5c6496a
Anuvab Chakravarty <106730134+Vanaub22@users.noreply.github.com>
2024-01-11 13:45:06 +0530

    Time: 36 ms (44.74%), Space: 22.3 MB (72.51%) - LeetHub

---
+++ 1447-simplified-fractions/1447-simplified-fractions.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<string> simplifiedFractions(int n) {
+        vector<string> res;
+        for(int i=n;i>1;i--) for(int j=1;j<i;j++) if(gcd(i,j)==1) res.push_back(to_string(j)+"/"+to_string(i));
+        return res;
+    }
+};
