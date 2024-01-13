173148afb3606ef366ce9a09b1f725611ea5fe83
Anuvab Chakravarty <106730134+Vanaub22@users.noreply.github.com>
2024-01-13 11:11:19 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (92.99%) - LeetHub

--- 1276-number-of-burgers-with-no-waste-of-ingredients/1276-number-of-burgers-with-no-waste-of-ingredients.cpp
+++ 1276-number-of-burgers-with-no-waste-of-ingredients/1276-number-of-burgers-with-no-waste-of-ingredients.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     // Can be solved in O(N) using a simple while loop
-    // This is the O(1) solution using Maths
+    // I am solving in O(1) T.C. using Maths
     vector<int> numOfBurgers(int ts, int cs) {
         int j=(ts-cs*2)/2; // The required no. of jumbo burgers to utilize all tomato slices
         return (j<0 || j*4+(cs-j)*2!=ts || cs-j<0)?vector<int>{}:vector<int>{j,cs-j};
