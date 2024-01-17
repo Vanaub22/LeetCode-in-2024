25ecafc5eb2299734515de3d052f06d11f3b0673
Anuvab Chakravarty <106730134+Vanaub22@users.noreply.github.com>
2024-01-17 12:32:34 +0530

    Time: 2 ms (63.07%), Space: 6.1 MB (100.00%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.cpp
@@ -5,7 +5,7 @@ public:
         // This is 1 + (4 + 8 + 12 + ... up to n - 1 terms) = 1 + 4 (1 + 2 + 3 + ... n - 1)
         // Sn = 1 + 4(n - 1)(n - 1 + 1) / 2 => Sn = 1 + 2n(n - 1)
         // The sum of the series (Sn) can be expressed as 1 + 2 * n * (n - 1)
-        return 1+(long long)2*n*(n-1);
+        return (long long)2*n*(n-1)+1;
 
     }
 };
