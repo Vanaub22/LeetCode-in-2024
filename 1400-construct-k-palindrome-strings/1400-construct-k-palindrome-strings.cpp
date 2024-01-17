0af4c07470fe9566fe5f0ce4d4ca36100e34f5a0
Anuvab Chakravarty <106730134+Vanaub22@users.noreply.github.com>
2024-01-17 11:42:59 +0530

    Time: 38 ms (39.78%), Space: 12.7 MB (11.44%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
@@ -4,9 +4,9 @@ public:
         if(s.length()<k) return false;
         unordered_map<char,int> freq;
         for(char c:s) freq[c]++;
-        int odd=0;
+        int odd_occ=0;
         // Number of odd frequency characters = Minimum number of palindromes that must be made
-        for(auto& it:freq) if(it.second%2) odd++;
-        return odd<=k;
+        for(auto& it:freq) if(it.second%2) odd_occ++;
+        return odd_occ<=k;
     }
 };
