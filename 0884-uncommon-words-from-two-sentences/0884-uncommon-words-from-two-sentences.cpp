4dab2f6b9fbf4d75b4419a297d0d241652df76eb
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-01-19 13:34:51 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (83.43%) - LeetHub

---
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        vector<string> uncommon;
+        s1+=" ";
+        s2+=" ";
+        unordered_map<string,int> freq;
+        string word="";
+        for(char c:s1) {
+            if(c!=' ') word+=c;
+            else {
+                cout<<word<<" ";
+                freq[word]++;
+                word="";
+            }
+        }
+        for(char c:s2) {
+            if(c!=' ') word+=c;
+            else {
+                cout<<word<<" ";
+                freq[word]++;
+                word="";
+            }
+        }
+        for(auto it:freq) if(it.second==1) uncommon.push_back(it.first);
+        return uncommon;
+    }
+};
