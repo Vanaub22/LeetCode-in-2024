49be7a5f1a8a080e6e4a5270b107c5e6adf1a72d
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-01-19 17:05:43 +0530

    Time: 7 ms (96.61%), Space: 15.4 MB (65.80%) - LeetHub

--- 0748-shortest-completing-word/0748-shortest-completing-word.cpp
+++ 0748-shortest-completing-word/0748-shortest-completing-word.cpp
@@ -5,7 +5,7 @@ public:
         for(char c:lp) if(isalpha(c)) lp_f[tolower(c)-'a']++;
         int shortest=INT_MAX;
         string res;
-        for(auto word:words) {
+        for(auto& word:words) {
             int f[26]={0};
             for(char c:word) f[tolower(c)-'a']++;
             if(completing(lp_f,f) && word.size()<shortest) {
