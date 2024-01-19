c169eb1a70c4f25ee81febca96714e2bad2ec12c
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-01-19 23:37:50 +0530

    Time: 262 ms (60.04%), Space: 18.2 MB (6.55%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    // Recursive solution gives TLE => Going for memoization
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int m=matrix.size(),n=matrix[0].size();
+        vector<vector<int>> memo(m,vector<int>(n,INT_MAX)); // To store cached values
+        int res=INT_MAX;
+        for(int i=0;i<n;i++) res=min(res,findFallSum(0,i,matrix,memo,m,n));
+        return res; 
+    }
+    // Helper fn. to perform the memoization
+    int findFallSum(int r, int c, vector<vector<int>>& mat, vector<vector<int>>& memo, int m, int n) {
+        if(memo[r][c]!=INT_MAX) return memo[r][c];
+        if(r==m-1) return memo[r][c]=mat[r][c];
+        int min_fall=INT_MAX;
+        for(int col=0;col<=n-1;col++) if(col!=c) min_fall=min(min_fall,findFallSum(r+1,col,mat,memo,m,n));
+        return memo[r][c]=mat[r][c]+min_fall;
+    }
+};
