210de78acae2d76366dbe4f46a514516bae4f09d
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-01-23 04:06:52 +0530

    Time: 11 ms (56.20%), Space: 7.1 MB (12.81%) - LeetHub

---
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+        int cnt=0;
+        for(int i=left;i<=right;i++) if(isPrime(countSetBits(i))) cnt++;
+        return cnt;
+    }
+    int countSetBits(int x) {
+        int cnt=0;
+        while(x>0) {
+            cnt+=x&1;
+            x>>=1;
+        }
+        return cnt;
+    }
+    bool isPrime(int x) {
+        if(x<=1) return false;
+        int cnt=0;
+        for(int i=2;i<=x/2;i++) if(x%i==0) cnt++;
+        return !cnt;
+    }
+};
