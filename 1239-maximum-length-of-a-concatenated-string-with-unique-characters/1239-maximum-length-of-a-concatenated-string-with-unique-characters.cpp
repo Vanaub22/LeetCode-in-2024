41d64af38540d9c154df5c636d7fff74a4ad5407
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-01-23 10:55:31 +0530

    Time: 301 ms (19.75%), Space: 124.4 MB (18.82%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxLength(vector<string>& arr) {
+        int res=0;
+        dfs(0,"",arr,arr.size(),res);
+        return res;
+    }
+    // Helper fn. to perform DFS and explore all possible subsequences
+    void dfs(int idx, string curr, vector<string>& arr, int n, int& res) {
+        if(hasUniqueChars(curr)) res=max(res,(int)curr.size());
+        if(!hasUniqueChars(curr) || idx==n) return;
+        for(int i=idx;i<n;i++) dfs(i+1,curr+arr[i],arr,n,res);
+    }
+    // To check if the characters in the current string are unique or not
+    bool hasUniqueChars(string& s) {
+        set<char> st;
+        for(char c:s) if(st.find(c)!=st.end()) return false;
+        else st.insert(c);
+        return true;
+    }
+};
