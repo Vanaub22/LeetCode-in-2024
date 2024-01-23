aaea9c6f37cddf4c31d8a603b7cd8cbe7acc3913
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-01-23 10:57:13 +0530

    Time: 161 ms (36.92%), Space: 68.8 MB (32.06%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -7,8 +7,8 @@ public:
     }
     // Helper fn. to perform DFS and explore all possible subsequences
     void dfs(int idx, string curr, vector<string>& arr, int n, int& res) {
-        if(hasUniqueChars(curr)) res=max(res,(int)curr.size());
-        if(!hasUniqueChars(curr) || idx==n) return;
+        if(!hasUniqueChars(curr)) return;
+        res=max(res,(int)curr.size());
         for(int i=idx;i<n;i++) dfs(i+1,curr+arr[i],arr,n,res);
     }
     // To check if the characters in the current string are unique or not
