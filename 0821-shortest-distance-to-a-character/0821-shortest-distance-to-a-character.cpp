359ff63082775ddc8391bab118113aad704e51f9
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-01-25 00:32:46 +0530

    Time: 5 ms (28.54%), Space: 8 MB (6.64%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=0;i<n;i++) if(s[i]==c) occurrences.push_back(i);
         vector<int> answer(n);
         for(int i=0;i<n;i++) {
-            int d=INT_MAX,idx=0;
+            int d=n,idx=0;
             for(int occurrence:occurrences) if(abs(occurrence-i)<=d) {
                 d=abs(occurrence-i);
                 idx=occurrence;
