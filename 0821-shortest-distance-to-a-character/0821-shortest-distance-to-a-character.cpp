70f29c2ce9e96b230b5ed21dd72f27456bb61ee5
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-01-25 00:31:29 +0530

    Time: 4 ms (46.79%), Space: 8.1 MB (6.64%) - LeetHub

---
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> shortestToChar(string s, char c) {
+        int n=s.size();
+        vector<int> occurrences;
+        for(int i=0;i<n;i++) if(s[i]==c) occurrences.push_back(i);
+        vector<int> answer(n);
+        for(int i=0;i<n;i++) {
+            int d=n,idx=-1;
+            for(int occurrence:occurrences) if(abs(occurrence-i)<=d) {
+                d=abs(occurrence-i);
+                idx=occurrence;
+            }
+            answer[i]=d;
+        }
+        return answer;
+    }
+};
