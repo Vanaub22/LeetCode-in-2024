6d6e6759df0162ca76f5cf751925ea0001df7ec1
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-01-26 13:51:00 +0530

    Time: 17 ms (77.45%), Space: 11.8 MB (9.18%) - LeetHub

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int appendCharacters(string s, string t) {
-        int i=0,ptr=0;
-        while(i<s.size()) if(s[i++]==t[ptr]) ptr++;
-        return t.size()-ptr;
+        int i=0,j=0;
+        while(i<s.size()) if(s[i++]==t[j]) j++;
+        return t.size()-j;
     }
 };
