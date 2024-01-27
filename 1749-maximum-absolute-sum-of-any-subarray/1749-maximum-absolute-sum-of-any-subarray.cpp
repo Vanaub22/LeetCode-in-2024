13be0e17aff0270d8bf4c10a14791aa0ae826571
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-01-27 10:52:37 +0530

    Time: 49 ms (60.96%), Space: 43.7 MB (14.25%) - LeetHub

---
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    // Kadane's Algorithm
+    int maxAbsoluteSum(vector<int>& nums) {
+        int msub=0,psub=0,max_pos=INT_MIN,min_neg=INT_MAX;
+        for(int i=0;i<nums.size();i++) {
+            psub+=nums[i];
+            msub+=nums[i];
+            if(psub>max_pos) max_pos=max(max_pos,psub);
+            if(msub<min_neg) min_neg=min(min_neg,msub);
+            if(psub<0) psub=0;
+            if(msub>0) msub=0;
+        }
+        return max_pos>-min_neg?max_pos:-min_neg;
+    }
+};
