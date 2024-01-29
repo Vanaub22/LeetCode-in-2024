379083cd765d523e0d0893869850443c5b342bce
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-01-29 23:42:58 +0530

    Time: 987 ms (5.04%), Space: 42.7 MB (16.30%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.cpp
@@ -3,17 +3,16 @@ public:
     int maxTurbulenceSize(vector<int>& arr) {
         int ans=0;
         for(int i=0;i<arr.size();i++) {
-            int w_end=i,comp=-1; // -1: Not assigned | 0: decreasing | 1: increasing
+            int w_end=i,comp=-1; // -1 => Not assigned
             while(w_end<arr.size()) {
-                // Checking for turbulence from the second element of the subarray
                 if(w_end-i!=0) {
                     if(arr[w_end-1]==arr[w_end]) break;
                     else if(arr[w_end-1]>arr[w_end]) {
-                        if(comp==0) break; // No turbulence
+                        if(comp==0) break;
                         else comp=0;
                     }
                     else {
-                        if(comp==1) break; // No turbulence
+                        if(comp==1) break;
                         comp=1;
                     }
                 }
