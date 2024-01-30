a485b6ccf15bc3766fa38103cfc56d28fbf0ef98
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-01-31 00:42:25 +0530

    Time: 25 ms (73.29%), Space: 31.3 MB (53.62%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     // Hashing prefix sums to reduce T.C. to O(N)
-    int numSubarraysWithSum(vector<int>& x, int goal) {
-        int n=x.size(),ans=0;
-        unordered_map<int,int> umap;
-        umap[0]++; // Initial prefix sum
+    int numSubarraysWithSum(vector<int>& x, int g) {
+        int n=x.size(),ans=0,pref[30000]={0};
+        pref[0]++; // Initial prefix sum
         for(int i=0;i<n;i++) {
             if(i) x[i]+=x[i-1];
-            if(x[i]>=goal) ans+=umap[x[i]-goal];
-            umap[x[i]]++;
+            if(x[i]>=g) ans+=pref[x[i]-g];
+            pref[x[i]]++;
         }
         return ans;
     }
