a628c4966fac3ad4dac928f30a99c0c32667e8b3
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-02-04 00:08:43 +0530

    Time: 76 ms (52.86%), Space: 62.1 MB (17.86%) - LeetHub

--- 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.cpp
+++ 0659-split-array-into-consecutive-subsequences/0659-split-array-into-consecutive-subsequences.cpp
@@ -6,17 +6,18 @@ public:
         for(int i:nums) freq[i]++;
         for(int i:nums) {
             if(reqd[i] && freq[i]) {
-                reqd[i]--;
+                reqd[i]--; 
                 freq[i]--;
-                reqd[i+1]++;
+                reqd[i+1]++; // Creating a need for the next element of subsequence
             }
             else {
-                if(!freq[i]) continue;
+                if(!freq[i]) continue; // All occurrences have been chosen for some subsequence
                 if(freq[i] && freq[i+1] && freq[i+2]) {
+                    // Creating a new subsequence having a minimum of 3 elements
                     freq[i]--;
                     freq[i+1]--;
                     freq[i+2]--;
-                    reqd[i+3]++;
+                    reqd[i+3]++; // Creating a need for the next element for this subsequence
                 }
                 else return false;
             }
