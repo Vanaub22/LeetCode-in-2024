2ebec4e5e323a9daa356c83967b7f5551af26a96
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-02-06 03:37:11 +0530

    Time: 106 ms (93.38%), Space: 62.8 MB (26.26%) - LeetHub

---
+++ 1472-design-browser-history/1472-design-browser-history.cpp
@@ -0,0 +1,51 @@
+// Browser history will be implemented using a Doubly Linked List
+// Defining custom class for nodes of DLL
+class Node {
+public:
+    Node* prev=nullptr,*next=nullptr;
+    string url;
+    Node(string url, Node* prev) {
+        this->prev=prev;
+        this->url=url;
+    }
+};
+
+class BrowserHistory {
+public:
+    Node* curr=nullptr;
+    BrowserHistory(string homepage) {
+        curr=new Node(homepage,nullptr);
+    }
+    
+    void visit(string url) {
+        Node* visited=new Node(url,curr);
+        curr->next=visited;
+        curr=curr->next;
+    }
+    
+    string back(int steps) {
+        while(steps) {
+            if(curr->prev) curr=curr->prev;
+            else break;
+            steps--;
+        }
+        return curr->url;
+    }
+    
+    string forward(int steps) {
+        while(steps) {
+            if(curr->next) curr=curr->next;
+            else break;
+            steps--;
+        }
+        return curr->url;
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
