80cfeb20bb77186328339186da4d7e4dfbedbd61
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-02-06 02:56:06 +0530

    Time: 6 ms (72.36%), Space: 8.4 MB (47.60%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.length()>s2.length()) return false;
+        vector<int> freq(26,0),w_freq(26,0);
+        for(char c:s1) freq[c-'a']++;
+        int n=s2.length(),left=0,right=s1.length()-1;
+        for(int i=left;i<=right;i++) w_freq[s2[i]-'a']++;
+        while(right<n) {
+            if(freq==w_freq) return true;
+            else {
+                right++;
+                if(right>n-1) break;
+                else w_freq[s2[right]-'a']++;
+                w_freq[s2[left++]-'a']--;
+            }
+        }
+        return false;
+    }
+};
