5702e7d1443a779a02b97bfeaa9f31532c6fa5ed
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-02-08 03:15:17 +0530

    Time: 87 ms (97.31%), Space: 94.3 MB (54.48%) - LeetHub

--- 0565-array-nesting/0565-array-nesting.cpp
+++ 0565-array-nesting/0565-array-nesting.cpp
@@ -6,7 +6,7 @@ public:
         return ans;
     }
     void dfs(vector<int>& nums, int k, int curr, int& ans) {
-        if(nums[k]>=0) {
+        if(nums[k]!=-1) {
             int nesting_idx=nums[k];
             nums[k]=-1; // Marking as visited
             dfs(nums,nesting_idx,curr+1,ans);
