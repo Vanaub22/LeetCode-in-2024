95841995c14c58fe831361467d11b275dd10df99
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-02-08 03:14:26 +0530

    Time: 119 ms (42.38%), Space: 94.4 MB (54.48%) - LeetHub

---
+++ 0565-array-nesting/0565-array-nesting.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int arrayNesting(vector<int>& nums) {
+        int ans=0;
+        for(int i=0;i<nums.size();i++) if(nums[i]!=1) dfs(nums,i,0,ans);
+        return ans;
+    }
+    void dfs(vector<int>& nums, int k, int curr, int& ans) {
+        if(nums[k]!=-1) {
+            int nesting_idx=nums[k];
+            nums[k]=-1; // Marking as visited
+            dfs(nums,nesting_idx,curr+1,ans);
+        }
+        else {
+            ans=max(ans,curr);
+            return;
+        }
+    }
+};
