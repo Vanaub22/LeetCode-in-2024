5a6e93fd7a568adde3d7526d676bb64414d30a23
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-02-08 19:51:29 +0530

    Time: 15 ms (49.32%), Space: 10.5 MB (60.02%) - LeetHub

--- 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.cpp
@@ -4,17 +4,13 @@ public:
         int ops=0;
         bool one=false;
         for(int i=0;i<target.size();i++) {
-            if(one) {
-                if(target[i]=='0') {
-                    one=false;
-                    ops++;
-                } 
-            }
-            else {
-                if(target[i]=='1') {
-                    one=true;
-                    ops++;
-                }
+            if(one && target[i]=='0') {
+                one=false;
+                ops++;
+            } 
+            else if(!one && target[i]=='1') {
+                one=true;
+                ops++;
             }
         }
         return ops;  
