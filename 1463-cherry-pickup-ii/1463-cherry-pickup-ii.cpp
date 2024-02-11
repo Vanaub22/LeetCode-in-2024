f4424b3999d10eb32fe9956c1d437128e32cf401
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-02-11 20:28:46 +0530

    Time: 52 ms (80.77%), Space: 19.4 MB (18.05%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -4,13 +4,11 @@
 class Solution {
 private:
     int solve(int i, int j1, int j2, int m, int n, v(v(int))& grid, v(v(v(int)))& memo) {
-        // Moving out of the boundary
-        if(j1<0 || j1>=n || j2<0 || j2>=n) return 0;
-        int cherries=j1==j2?grid[i][j1]:grid[i][j1]+grid[i][j2];
-        // Reaching the last row
+        if(j1<0 || j1>=n || j2<0 || j2>=n) return 0; // Moving out of the boundary
+        int cherries=j1==j2?grid[i][j1]:grid[i][j1]+grid[i][j2]; // Reaching the last row
         if(i==m-1) return cherries;
         if(memo[i][j1][j2]!=-1) return memo[i][j1][j2];
-        // Exploring all possible paths (9 possible combinations for each {i,j1} and {i,j2})
+        // Exploring all possible paths (9 possible combinations for each (i,j1) and (i,j2))
         int maxCherries=0;
         for(int dj1=-1;dj1<2;dj1++) {
             for(int dj2=-1;dj2<2;dj2++) {
