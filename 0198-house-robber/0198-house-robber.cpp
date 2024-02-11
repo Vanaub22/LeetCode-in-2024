04c0657d8a184efd00d4b18efceea63e7aa2adb9
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-02-12 03:38:19 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (71.89%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,17 @@
+// Recurive approach involving take / not-take optimized using 1-D DP on subsequences (Memoization)
+class Solution {
+private:
+    int solve(int i, vector<int>& nums, int memo[]) {
+        if(!i) return nums[0];
+        if(i<0) return 0;
+        if(memo[i]!=-1) return memo[i];
+        int take=nums[i]+solve(i-2,nums,memo),not_take=solve(i-1,nums,memo);
+        return memo[i]=max(take,not_take);
+    }
+public:
+    int rob(vector<int>& nums) {
+        int memo[nums.size()];
+        memset(memo,-1,sizeof(memo));
+        return solve(nums.size()-1,nums,memo);
+    }
+};
