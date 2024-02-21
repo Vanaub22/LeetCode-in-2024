a1f7ce8a306b56dbd11024d217f26e59b078cbe1
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-02-21 08:42:54 +0530

    Time: 4 ms (79.27%), Space: 8.8 MB (26.31%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // 2 consecutive numbers would only differ in the LSB
+    // Bitwise AND-ing such pairs of numbers would only yield the smaller number
     int rangeBitwiseAnd(int left, int right) {
         while(left<right) right&=(right-1);
         return right;
