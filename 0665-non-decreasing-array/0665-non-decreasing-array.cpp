eb85cbb03b8cb22c6f5addfbb9ecdf3b8fc2592e
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-02-26 11:58:15 +0530

    Time: 19 ms (63.94%), Space: 29.4 MB (48.53%) - LeetHub

---
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int n=nums.size(),peaks=0;
+        for(int i=0;i<n-1;i++) {
+            if(nums[i]>nums[i+1]) {
+                if(i==0 || i==n-2)  peaks++; // Removal at the beginning or at the end
+                else {
+                    // Removal somewhere in the middle (Either nums[i] or nums[i+1])
+                    if(nums[i-1]<=nums[i+1] || nums[i]<=nums[i+2]) peaks++;
+                    else return false;
+                }
+            }
+        }
+        return n==1 || peaks<=1;
+    }
+};
