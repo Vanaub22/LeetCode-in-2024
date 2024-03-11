d7d7675262e50cc5bb08418f39dbb656f7e18c4b
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-03-11 06:43:58 +0530

    Time: 2 ms (53.95%), Space: 9.4 MB (6.35%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        unordered_map<char,int> weight;
+        for(int i=0;i<order.size();i++) weight[order[i]]=i;
+        sort(s.begin(),s.end(),[&](char& a, char& b) {
+            return weight[a]<weight[b];
+        });
+        return s;
+    }
+};
