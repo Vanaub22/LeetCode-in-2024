83d9da7e37431dbe39579ae7038a3b3e8509a85c
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-03-30 00:03:03 +0530

    Time: 119 ms (59.19%), Space: 91.8 MB (67.91%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     int maximumScore(vector<int>& nums, int k) {
         // Starting from the index k and then greedily expanding outwards using two pointers
-        int left=k,right=left,n=nums.size(),maxScore=-1,minimum=nums[k],w=1;
+        int left=k,right=left,n=nums.size(),maxScore=-1,minimum=nums[k];
         bool moveLeft=true;
         while(left>=0 || right<=n-1) {
-            maxScore=max(maxScore,minimum*w);
+            maxScore=max(maxScore,minimum*(right-left+1));
             if(!left && right==n-1) break;
-            else if(!left) minimum=min(minimum,nums[++right]);
+            if(!left) minimum=min(minimum,nums[++right]);
             else if(right==n-1) minimum=min(minimum,nums[--left]);
             else {
                 moveLeft=nums[left-1]>nums[right+1];
                 if(moveLeft) minimum=min(minimum,nums[--left]);
                 else minimum=min(minimum,nums[++right]);
             }
-            w++;
         }
         return maxScore;
     }
