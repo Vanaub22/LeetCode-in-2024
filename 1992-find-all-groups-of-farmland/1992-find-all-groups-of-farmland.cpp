7aa8e30a82b97dd3cf517824686c2175851d7b43
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-04-20 13:57:38 +0530

    Time: 93 ms (85.82%), Space: 71.8 MB (58.91%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -1,6 +1,17 @@
 class Solution {
+private:
+    // Helper DFS fn. to explore current piece of farmland and find the bottom-right corner
+    void exp(int i, int j, int& br_x, int& br_y, vector<vector<int>>& land, int m, int n) {
+        if(i<0 || i>m-1 || j<0 || j>n-1 || !land[i][j]) return;
+        land[i][j]=0; // Marking as visited
+        br_x=max(br_x,i);
+        br_y=max(br_y,j);
+        exp(i+1,j,br_x,br_y,land,m,n);
+        exp(i,j+1,br_x,br_y,land,m,n);
+        exp(i-1,j,br_x,br_y,land,m,n);
+        exp(i,j-1,br_x,br_y,land,m,n);
+    }
 public:
-    // Simple DFS Approach
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
         vector<vector<int>> res;
         int m=land.size(),n=land[0].size();
@@ -14,16 +25,5 @@ public:
             }
         }
         return res;
-    }
-    // Helper DFS fn. to explore current piece of farmland and find the bottom-right corner
-    void exp(int i, int j, int& br_x, int& br_y, vector<vector<int>>& land, int m, int n) {
-        if(i<0 || i>m-1 || j<0 || j>n-1 || !land[i][j]) return;
-        land[i][j]=0; // Marking as visited
-        br_x=max(br_x,i);
-        br_y=max(br_y,j);
-        exp(i+1,j,br_x,br_y,land,m,n);
-        exp(i,j+1,br_x,br_y,land,m,n);
-        exp(i-1,j,br_x,br_y,land,m,n);
-        exp(i,j-1,br_x,br_y,land,m,n);
     }
 };
