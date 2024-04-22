0b03a89892c3cdd06578762cf08ba3dd09438468
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-04-22 12:49:31 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (93.57%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,21 @@
+// 2-Pass Solution O(N) T.C. and O(1) S.C.
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n=s.length(),opened=0,closed=0,ans=0;
+        for(char c:s) {
+            if(c=='(') opened++;
+            else closed++;
+            if(opened==closed) ans=max(ans,opened*2);
+            else if(closed>opened) opened=closed=0;
+        }
+        opened=closed=0;
+        for(int i=n-1;i>=0;i--) {
+            if(s[i]=='(') opened++;
+            else closed++;
+            if(opened==closed) ans=max(ans,opened*2);
+            else if(opened>closed) opened=closed=0;
+        }
+        return ans;
+    }
+};
