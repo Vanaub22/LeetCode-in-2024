e1c91e6cb7116f76f8ed1fac544b7508e1a2d5c8
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-04-23 11:24:00 +0530

    Time: 121 ms (42.64%), Space: 83.8 MB (51.93%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,24 @@
+// Monotonic Stack Solution (Next Greater Element Logic)
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size(),maxArea=-1;
+        vector<int> leftWall(n),rightWall(n);
+        stack<int> stk;
+        for(int i=0;i<n;i++) {
+            while(!stk.empty() && heights[stk.top()]>=heights[i]) stk.pop();
+            if(stk.empty()) leftWall[i]=0;
+            else leftWall[i]=stk.top()+1;
+            stk.push(i);
+        }
+        while(!stk.empty()) stk.pop();
+        for(int i=n-1;i>=0;i--) {
+            while(!stk.empty() && heights[stk.top()]>=heights[i]) stk.pop();
+            if(stk.empty()) rightWall[i]=n-1;
+            else rightWall[i]=stk.top()-1;
+            stk.push(i);
+            maxArea=max(maxArea,(rightWall[i]-leftWall[i]+1)*heights[i]);
+        }
+        return maxArea;
+    }
+};
