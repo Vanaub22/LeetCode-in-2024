107b31e62a587cbe382545111b87b57c1ba1aab7
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-04-24 20:43:40 +0530

    Time: 47 ms (91.00%), Space: 31.9 MB (90.66%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,15 +1,17 @@
 
+// Optimized T.C. from O(N^3) to O(N^2) at the cost of O(N^2) space
+// O(N^3) solution passes all test cases
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        int maxNR=-1;
-        for(int i=0;i<n-1;i++) {
-            for(int j=i+1;j<n;j++) {
-                int cnt=0;
-                for(auto& road:roads) if(road[0]==i || road[1]==i || road[0]==j || road[1]==j) cnt++;
-                maxNR=max(maxNR,cnt);
-            }
+        vector<vector<bool>> directConnection(n,vector<bool>(n,false));
+        int degree[n],maxNR=-1;
+        bzero(degree,sizeof(degree));
+        for(auto& road:roads) {
+            directConnection[road[0]][road[1]]=directConnection[road[1]][road[0]]=true;
+            degree[road[0]]++,degree[road[1]]++;
         }
+        for(int i=0;i<n-1;i++) for(int j=i+1;j<n;j++) maxNR=max(maxNR,degree[i]+degree[j]-directConnection[i][j]);
         return maxNR;
     }
 };
