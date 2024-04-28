3b70af9739f4cfb80b1b2d749cb731907c973d01
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-04-28 12:28:09 +0530

    Time: 251 ms (33.95%), Space: 99.6 MB (51.73%) - LeetHub

---
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.cpp
@@ -0,0 +1,32 @@
+class Solution {
+private:
+    void dist(int root, int parent, vector<int> adjList[], auto& ans, auto& cnt) {
+        for(int node:adjList[root]) {
+            if(node==parent) continue;
+            dist(node,root,adjList,ans,cnt);
+            cnt[root]+=cnt[node];
+            ans[root]+=ans[node]+cnt[node];
+        }
+        cnt[root]++;
+    }
+    void calc(int root, int parent, vector<int> adjList[], int n, auto& ans, auto& cnt) {
+        for(int node:adjList[root]) {
+            if(node==parent) continue;
+            // At node, cnt[node] nodes will get closer and n - cnt[node] nodes will go further
+            ans[node]=ans[root]-cnt[node]+n-cnt[node];
+            calc(node,root,adjList,n,ans,cnt);
+        }
+    }
+public:
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        vector<int> adjList[n];
+        for(auto edge:edges) {
+            adjList[edge[0]].push_back(edge[1]);
+            adjList[edge[1]].push_back(edge[0]);
+        }
+        vector<int> ans(n,0),cnt(n,0);
+        dist(0,-1,adjList,ans,cnt);
+        calc(0,-1,adjList,n,ans,cnt);
+        return ans;
+    }
+};
