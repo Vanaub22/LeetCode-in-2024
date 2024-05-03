b4175bc103c10bdc3402688c621c1a185e20578a
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-05-03 09:00:24 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (93.86%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    int compareVersion(string v1, string v2) {
+    int compareVersion(string& v1, string& v2) {
         int i=0,j=0,l1=v1.length(),l2=v2.length();
         while(true) {
-            int r1=0,r2=0;
+            long long int r1=0,r2=0;
             while(i<l1 && v1[i]!='.') {
-                r1*=10;
-                r1+=v1[i]-'0';
+                r1=r1*10+v1[i]-'0';
                 i++;
             }
             while(j<l2 && v2[j]!='.') {
-                r2*=10;
-                r2+=v2[j]-'0';
+                r2=r2*10+v2[j]-'0';
                 j++;
             }
             if(r1<r2) return -1;
             else if(r1>r2) return 1;
             if(i>=l1 && j>=l2) break;
-            i++;
-            j++;
+            i++,j++;
         }
         return 0;
     }
