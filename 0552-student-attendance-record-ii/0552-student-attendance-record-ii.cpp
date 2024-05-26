1074b9ef9933dfd0bbb5ff42658462fb75177df4
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-05-26 11:44:43 +0530

    Time: 2010 ms (5.12%), Space: 536.5 MB (5.12%) - LeetHub

---
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -0,0 +1,18 @@
+#define MOD ((int)1e9+7)
+class Solution {
+private:
+    int solve(int n, int consecutive_lates, int days_absent, auto& memo) {
+        if(consecutive_lates>=3 || days_absent>=2) return 0;
+        if(!n) return 1;
+        if(memo[n][consecutive_lates][days_absent]!=-1) return memo[n][consecutive_lates][days_absent];
+        int cnt=solve(n-1,0,days_absent,memo);
+        cnt=(cnt+solve(n-1,consecutive_lates+1,days_absent,memo))%MOD;
+        cnt=(cnt+solve(n-1,0,days_absent+1,memo))%MOD;
+        return memo[n][consecutive_lates][days_absent]=cnt;
+    }
+public:
+    int checkRecord(int n) {
+        vector<vector<vector<int>>> memo(n+1,vector<vector<int>>(3,vector<int>(2,-1)));
+        return solve(n,0,0,memo);
+    }
+};
