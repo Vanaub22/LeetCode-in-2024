2c1b0d61351dd3209cf67152a8c6ee637f973695
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-05-31 12:41:00 +0530

    Time: 3 ms (96.61%), Space: 12.3 MB (87.16%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -4,7 +4,6 @@ public:
         int x=0,shift=0,a=0,b=0;
         for(int i:nums) x^=i; // Will yield a^b where a and b are the required numbers
         while(((x>>shift)&1)==0) shift++; // Finding the rightmost unequal bit between a and b
-        // Separating into two groups and performing XOR to obtain the numbers
         for(int i:nums) {
             if(((i>>shift)&1)==0) a^=i;
             else b^=i;
