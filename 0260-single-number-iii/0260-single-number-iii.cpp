5c394e9a7147b97d9cc5316ae244079dcff4f24e
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-05-31 12:40:47 +0530

    Time: 9 ms (44.18%), Space: 12.3 MB (87.16%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         int x=0,shift=0,a=0,b=0;
-        for(int i:nums) x^=i;
-        while(((x>>shift)&1)==0) shift++;
+        for(int i:nums) x^=i; // Will yield a^b where a and b are the required numbers
+        while(((x>>shift)&1)==0) shift++; // Finding the rightmost unequal bit between a and b
+        // Separating into two groups and performing XOR to obtain the numbers
         for(int i:nums) {
             if(((i>>shift)&1)==0) a^=i;
             else b^=i;
