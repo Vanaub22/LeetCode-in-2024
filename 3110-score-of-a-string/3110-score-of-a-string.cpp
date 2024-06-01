a1836e45aaa12f7b762f9443b5c517de74855b1b
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-06-01 10:00:04 +0530

    Time: 5 ms (27.27%), Space: 7.9 MB (42.17%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.cpp
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        int n=s.size(),ans=0;
-        for(int i=0;i<n-1;i++) ans+=abs((int)s[i]-(int)s[i+1]);
-        return ans;
+        int n=s.size(),score=0;
+        for(int i=0;i<n-1;i++) score+=abs((int)s[i]-(int)s[i+1]);
+        return score;
     }
 };
