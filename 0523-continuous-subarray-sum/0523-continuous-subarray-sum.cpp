e98d753fb54cf07c88d9dafac7914328f493fe66
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-06-08 07:46:05 +0530

    Time: 167 ms (86.37%), Space: 134.7 MB (59.36%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n=nums.size(),pfs=0,r;
+        if(n<=1) return false;
+        unordered_map<int,int> rem_index={{0,-1}};
+        for(int i=0;i<n;i++) {
+            pfs+=nums[i],r=pfs%k;
+            if(rem_index.find(r)==rem_index.end()) rem_index[r]=i;
+            else if(i-rem_index[r]>1) return true;
+        }
+        return false;
+    }
+};
