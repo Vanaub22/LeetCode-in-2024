6d799d13965ad7b423aaed5c63d484a1cad64651
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-06-09 09:11:08 +0530

    Time: 39 ms (65.39%), Space: 35.3 MB (18.1%) - LeetHub

---
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,21 @@
+// Optimized Solution (Typical Approach) involving Prefix Sum
+// O(N) T.C. and O(k) S.C.
+// Let p and q be 2 prefix sums at different points in the array
+// Let us assume that p and q have the same remainder r when divided by k
+// Therefore, p and q can be written as p = k x q1 + r and q = k x q2 + r
+// => p - q = k x (q1 - q2) => the subarray between p and q is divisible by k
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        // Converting to prefix sum array
+        int pf=0,ans=0;
+        unordered_map<int,int> rem={{0,1}};
+        for(int i:nums) {
+            pf+=i;
+            int r=pf%k;
+            if(r<0) r+=k;
+            ans+=rem[r]++;
+        }
+        return ans;
+    }
+};
