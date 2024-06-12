27afe1dc858b53f49f4dee0bfecf90061d184f96
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-06-12 06:34:00 +0530

    Time: 0 ms (100%), Space: 10.9 MB (6.16%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.cpp
+++ 1217-relative-sort-array/1217-relative-sort-array.cpp
@@ -6,8 +6,7 @@ public:
         sort(arr1.begin(),arr1.end(),[&](int& a, int& b) {
             unordered_map<int,int>::iterator itra=ump.find(a),itrb=ump.find(b);
             if(itra==ump.end() && itrb==ump.end()) return a<b;
-            else if(itra==ump.end()) return false;
-            else if(itrb==ump.end()) return true;
+            else if(itra==ump.end() || itrb==ump.end()) return itrb==ump.end();
             else return ump[a]<ump[b];
         });
         return arr1;
