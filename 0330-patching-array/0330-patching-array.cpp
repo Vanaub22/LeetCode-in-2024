c7fe796b3cd72fe880641ae5c2751c31ea0a1e01
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-06-16 07:04:06 +0530

    Time: 0 ms (100%), Space: 13.7 MB (82.46%) - LeetHub

--- 0330-patching-array/0330-patching-array.cpp
+++ 0330-patching-array/0330-patching-array.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int minPatches(vector<int>& nums, int n) {
         int cost=0,m=nums.size(),i=0;
-        long long int upper=0;
-        while(upper<n) {
-            if(i<m && nums[i]<=upper+1) {
-                upper+=nums[i];
+        long long int upperlim=0;
+        while(upperlim<n) {
+            if(i<m && nums[i]<=upperlim+1) {
+                upperlim+=nums[i];
                 i++;
             }
             else {
                 cost++;
-                upper+=upper+1;
+                upperlim+=upperlim+1;
             }
         }
         return cost;
