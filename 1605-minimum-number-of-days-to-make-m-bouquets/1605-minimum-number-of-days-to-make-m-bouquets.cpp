2bf09e83adbefe7d624cc4352961e3650785b870
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-06-19 09:31:22 +0530

    Time: 132 ms (6.65%), Space: 69 MB (28.78%) - LeetHub

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    int possibleBouquets(int d, auto& b, int k) {
+        int adj=0,ans=0,n=b.size();
+        for(int i=0;i<n;i++) {
+            if(b[i]<=d) adj++;
+            if(b[i]>d || i==n-1) {
+                ans+=adj/k;
+                adj=0;
+            }
+        }
+        return ans;
+    }
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int ans=-1,mid,n=bloomDay.size(),left=0,right=1e9;
+        while(left<=right) {
+            mid=left+(right-left)/2;
+            if(possibleBouquets(mid,bloomDay,k)>=m) {
+                right=mid-1;
+                ans=mid;
+            }
+            else left=mid+1;   
+        }     
+        return ans;
+    }
+};
