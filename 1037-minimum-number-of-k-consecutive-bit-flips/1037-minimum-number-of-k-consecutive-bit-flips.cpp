656ab0fd0c6601806ee92596a094f690afa772a5
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-06-24 08:11:13 +0530

    Time: 94 ms (46.31%), Space: 113.3 MB (10.25%) - LeetHub

--- 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.cpp
+++ 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -2,20 +2,19 @@ class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
         int n=nums.size(),minFlips=0;
+        auto flip=[&nums](int flips, int i)->int {
+            return flips%2?!nums[i]:nums[i];
+        };
         vector<int> prefixFlips(n+1,0);
         for(int i=0;i<n;i++) {
             if(i) prefixFlips[i]+=prefixFlips[i-1];
             if(i<n-k+1) {
-                int bit=prefixFlips[i]%2?!nums[i]:nums[i];
-                if(bit) continue;
+                if(flip(prefixFlips[i],i)) continue;
                 minFlips++;
                 prefixFlips[i]++,prefixFlips[i+k]--;
             }
         }
-        for(int i=0;i<n;i++) {
-            int bit=prefixFlips[i]%2?!nums[i]:nums[i];
-            if(!bit) return -1;
-        }
+        for(int i=0;i<n;i++) if(!flip(prefixFlips[i],i)) return -1;
         return minFlips;
     }
 };
