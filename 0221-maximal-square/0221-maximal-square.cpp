67d2890cbfdc54c2d6ca8d3fa4d4cab5ab63999d
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-07-10 04:13:13 +0530

    Time: 85 ms (17.68%), Space: 25.7 MB (9.99%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -3,13 +3,13 @@ public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int m=matrix.size(),n=matrix[0].size(),ans=-1;
         vector<vector<int>> memo=vector<vector<int>>(m,vector<int>(n,-1));
-        function<int(int,int)> helper=[&helper,&matrix,&memo,m,n](int i, int j)->int{
+        function<int(int,int)> dp=[&dp,&matrix,&memo,m,n](int i, int j)->int{
             if(i>=m || j>=n) return 0;
             if(memo[i][j]!=-1) return memo[i][j];
-            int calcRight=helper(i,j+1),calcDown=helper(i+1,j),calcDiag=helper(i+1,j+1);
+            int calcRight=dp(i,j+1),calcDown=dp(i+1,j),calcDiag=dp(i+1,j+1);
             return memo[i][j]=matrix[i][j]-'0'?1+min({calcRight,calcDown,calcDiag}):0;
         };
-        helper(0,0);
+        dp(0,0);
         for(auto& row:memo) ans=max(ans,(int)pow(*max_element(row.begin(),row.end()),2));
         return ans;
     }
