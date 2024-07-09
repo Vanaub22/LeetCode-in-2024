f856efe48aa1921bdf37af5a60fd88fb115999ad
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-07-10 04:12:22 +0530

    Time: 89 ms (15.94%), Space: 25.8 MB (9.99%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int m=matrix.size(),n=matrix[0].size(),ans=-1;
-        vector<vector<int>> memo=vector<vector<int>>(m+1,vector<int>(n+1,-1));
+        vector<vector<int>> memo=vector<vector<int>>(m,vector<int>(n,-1));
         function<int(int,int)> helper=[&helper,&matrix,&memo,m,n](int i, int j)->int{
-            if(i>=m || j>=n) return memo[i][j]=0;
+            if(i>=m || j>=n) return 0;
             if(memo[i][j]!=-1) return memo[i][j];
             int calcRight=helper(i,j+1),calcDown=helper(i+1,j),calcDiag=helper(i+1,j+1);
             return memo[i][j]=matrix[i][j]-'0'?1+min({calcRight,calcDown,calcDiag}):0;
