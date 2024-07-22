7ac8e6db932a15103ce22ebfdca8deb3ff44ef80
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-07-22 11:59:47 +0530

    Time: 30 ms (55.87%), Space: 28.1 MB (23.37%) - LeetHub

---
+++ 2502-sort-the-people/2502-sort-the-people.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        vector<string> ans;
+        priority_queue<pair<int,string>> pq;
+        for(int i=0;i<heights.size();i++) pq.push({heights[i],names[i]});
+        while(!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
