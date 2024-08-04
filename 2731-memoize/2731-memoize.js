3795a553b79451fe479767f90344b638f74eec3c
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-08-04 10:38:02 +0530

    Time: 310 ms (42.37%), Space: 86.8 MB (87.89%) - LeetHub

---
+++ 2731-memoize/2731-memoize.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    let cache=[];
+    return function(...args) {
+        const key=JSON.stringify(args);
+        return cache[key]=key in cache?cache[key]:fn(...args);
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
