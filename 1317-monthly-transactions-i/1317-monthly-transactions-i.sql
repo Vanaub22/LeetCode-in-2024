2e4fad42d7ddca7834d3c7bf204a38f65ce3f109
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-08-09 09:33:27 +0530

    Time: 609 ms (55.53%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -1,8 +1,8 @@
-# Write your MySQL query statement below
-select date_format(trans_date,'%Y-%m') as month, country,
+/* Write your PL/SQL query statement below */
+select to_char(trans_date,'yyyy-mm') as month, country,
 count(*) as trans_count,
 sum(case when state='approved' then 1 else 0 end) as approved_count,
 sum(amount) as trans_total_amount,
 sum(case when state='approved' then amount else 0 end) as approved_total_amount
 from transactions
-group by month, country;
+group by to_char(trans_date,'yyyy-mm'), country;
