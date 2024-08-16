fd999a035a0cd4f9f472bf52096ba1402beee9ef
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-08-16 13:03:34 +0530

    Time: 221 ms (83.49%), Space: 107.9 MB (57.55%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -5,20 +5,10 @@ public:
         pair<int,int> min1={-1,INT_MAX},min2={-1,INT_MAX},max1={-1,INT_MIN},max2={-1,INT_MIN};
         for(int i=0;i<m;i++) {
             int n=arrays[i].size(),a=arrays[i][0],b=arrays[i][n-1];
-            if(a<min1.second) {
-                min2=min1;
-                min1={i,a};
-            }
-            else if(a<min2.second && a>=min1.second) {
-                min2={i,a};
-            }
-            if(b>max1.second) {
-                max2=max1;
-                max1={i,b};
-            }
-            else if(b>max2.second && b<=max1.second) {
-                max2={i,b};
-            }
+            if(a<min1.second) min2=min1,min1={i,a};
+            else if(a<min2.second && a>=min1.second) min2={i,a};
+            if(b>max1.second) max2=max1,max1={i,b};
+            else if(b>max2.second && b<=max1.second) max2={i,b};
         }
         if(min1.first!=max1.first && min1.first!=-1 && max1.first!=-1) d1=max1.second-min1.second;
         if(min1.first!=max2.first && min1.first!=-1 && max2.first!=-1) d2=max2.second-min1.second;
