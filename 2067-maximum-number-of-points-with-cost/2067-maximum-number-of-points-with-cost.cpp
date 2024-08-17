798c34eda96a4cf238643e7e864fa0798c7b1104
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-08-17 12:33:58 +0530

    Time: 176 ms (75.75%), Space: 97.8 MB (73.57%) - LeetHub

---
+++ 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long maxPoints(vector<vector<int>>& points) {
+        int m=points.size(),n=points[0].size();
+        long long curr;
+        vector<long long> maxPts(n),right(n);
+        for(auto row:points) {
+            curr=0;
+            for(int j=n-1;j>=0;j--) {
+                curr=max(curr,maxPts[j]);
+                right[j]=curr--;
+            }
+            curr=0;
+            for(int j=0;j<n;j++){
+                curr=max(curr,maxPts[j]);
+                maxPts[j]=max(curr--,right[j])+row[j];
+            }
+        }
+        return *max_element(maxPts.begin(),maxPts.end());
+    }
+};
