0b0a1a0089214b4b6a09cdfaacfdbbd16dfd599b
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-08-18 09:52:49 +0530

    Time: 7 ms (51.38%), Space: 9.1 MB (83.32%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int nthUglyNumber(int n) {
         int dp[1690];
-        dp[0]=1; // 1 is considered an ugly number
+        dp[0]=1;
         int p2=0,p3=0,p5=0;
         for(int i=1;i<1690;i++) {
             dp[i]=min({dp[p2]*2,dp[p3]*3,dp[p5]*5});
