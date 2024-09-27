1b3a92af994ef4db62b94275a7e4dd8dec64f953
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-09-27 12:44:50 +0530

    Time: 73 ms (88.84%), Space: 37.7 MB (99.37%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.cpp
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -1,16 +1,9 @@
 class MyCalendarTwo {
-private:
-    bool checkOverlap(int s1, int e1, int s2, int e2) {
-        return max(s1,s2)<min(e1,e2);
-    }
-    auto findOverlap(int s1, int e1, int s2, int e2) {
-        return make_pair(max(s1,s2),min(e1,e2));
-    }
 public:
     vector<pair<int,int>> sBooking,dBooking;
     bool book(int start, int end) {
-        for(auto& [s,e]:dBooking) if(checkOverlap(start,end,s,e)) return false;
-        for(auto& [s,e]:sBooking) if(checkOverlap(start,end,s,e)) dBooking.emplace_back(findOverlap(start,end,s,e));
+        for(auto& [s,e]:dBooking) if(max(start,s)<min(end,e)) return false;
+        for(auto& [s,e]:sBooking) if(max(start,s)<min(end,e)) dBooking.emplace_back(make_pair(max(start,s),min(end,e)));
         sBooking.emplace_back(make_pair(start,end));
         return true;
     }
