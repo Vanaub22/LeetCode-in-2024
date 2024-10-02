1817f5a0837c6fa4f0bbb8919d86b723461e1204
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-10-02 06:47:46 +0530

    Time: 60 ms (81.51%), Space: 44.4 MB (38.29%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -4,17 +4,17 @@ public:
         if(arr.empty()) return {};
         unordered_map<int,int> ranking;
         vector<int> tmp(arr.begin(),arr.end()),ans;
-        sort(arr.begin(),arr.end());
-        ranking[arr[0]]=1;
-        int n=arr.size(),rank=1,prev=arr[0];
+        sort(tmp.begin(),tmp.end());
+        ranking[tmp[0]]=1;
+        int n=tmp.size(),rank=1,prev=tmp[0];
         for(int i=1;i<n;i++) {
-            if(prev!=arr[i]) {
-                ranking[arr[i]]=++rank;
-                prev=arr[i];
+            if(prev!=tmp[i]) {
+                ranking[tmp[i]]=++rank;
+                prev=tmp[i];
             }
-            else ranking[arr[i]]=rank;
+            else ranking[tmp[i]]=rank;
         }
-        for(int i:tmp) ans.emplace_back(ranking[i]);
+        for(int i:arr) ans.emplace_back(ranking[i]);
         return ans;
     }
 };
