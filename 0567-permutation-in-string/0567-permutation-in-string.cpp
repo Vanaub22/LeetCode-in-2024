3739aee59d471ca7cd98b587dd5af973c1a32c41
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-10-05 10:15:27 +0530

    Time: 9 ms (48.5%), Space: 8.8 MB (80.36%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         if(s1.length()>s2.length()) return false;
+        // Using vectors so that they can be compared using overloaded '==' operator
         vector<int> freq(26,0),w_freq(26,0);
         for(char c:s1) freq[c-'a']++;
         int n=s2.length(),left=0,right=s1.length()-1;
