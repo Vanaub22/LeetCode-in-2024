7ea00f986e12cb433d33944a0643076a36d1bbb8
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-10-14 09:04:59 +0530

    Time: 184 ms (84.04%), Space: 79.6 MB (64.84%) - LeetHub

--- 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -5,7 +5,7 @@ public:
         long long score=0;
         for(int i:nums) pq.push(i);
         for(int i=0;i<k;i++){
-            if(pq.empty()) return(score);
+            if(pq.empty()) return score;
             score+=pq.top();
             pq.push(ceil((double)pq.top()/3));
             pq.pop();
