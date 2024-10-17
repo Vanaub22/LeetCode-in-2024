95eb742f3a526ef66883b90bc9bb847b70a05604
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-10-17 10:22:05 +0530

    Time: 3 ms (38.18%), Space: 7.8 MB (12.29%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int maximumSwap(int num) {
         string best=to_string(num),curr=best;
-        sort(best.rbegin(),best.rend()); // Best possible result
+        sort(best.rbegin(),best.rend());
         int n=curr.size();
         for(int i=0;i<n;i++) {
             if(best[i]!=curr[i]) {
-                // find_last_of implementation
                 int j=n-1;
                 for(;j>=0;j--) if(curr[j]==best[i]) break;
                 swap(curr[j],curr[i]);
