0a339ef51a99fef8ac3aa3424371e4b07e370dd1
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-11-02 07:22:47 +0530

    Time: 0 ms (100%), Space: 8.1 MB (83.91%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.cpp
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     bool isCircularSentence(string sentence) {
-        if(sentence[0]!=sentence[sentence.length()-1])
-        return(false);
-        for(int i=0;i<sentence.length();i++)
-        if(sentence[i]==' ' && sentence[i-1]!=sentence[i+1])
-        return(false);
-        return(true);
+        int n=sentence.length();
+        if(sentence[0]!=sentence[n-1]) return false;
+        for(int i=0;i<n;i++) if(sentence[i]==' ' && sentence[i-1]!=sentence[i+1]) return false;
+        return true;
     }
 };
