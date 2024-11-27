418830e50404316e270ded6ee332ccde836c83d1
Anuvab Chakravarty <anucbs2018@gmail.com>
2024-11-28 02:13:44 +0530

    Time: 288 ms (37.39%), Space: 126.8 MB (55.07%) - LeetHub

--- 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
+++ 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
-        unordered_map<int,vector<pair<int,int>>> g;
-        for(int i=0;i<n-1;i++) g[i].emplace_back(i+1,1);
+        vector<pair<int,int>> g[n+1];
+        for(int i=0;i<n-1;i++) g[i].push_back({i+1,1});
         vector<int> answer;
         for(auto& query:queries) {
             int u=query[0],v=query[1];
-            g[u].emplace_back(v,1);
-            int shortestPath=dijkstra(0,n - 1,g, n);
-            answer.push_back(shortestPath);
+            g[u].push_back({v,1});
+            answer.push_back(dijkstra(0,n-1,g,n));
         }
         return answer;
     }
-
 private:
-    int dijkstra(int start, int end, unordered_map<int, vector<pair<int, int>>>& graph, int n) {
-        vector<int> distances(n, INT_MAX);
-        distances[start] = 0;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq; // min-heap
-        pq.push({0, start});
-        while (!pq.empty()) {
-            int currentDist = pq.top().first;
-            int current = pq.top().second;
+    int dijkstra(int start, int end, vector<pair<int,int>> g[], int n) {
+        vector<int> distances(n,INT_MAX);
+        distances[start]=0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>> pq;
+        pq.push({0,start});
+        while(!pq.empty()) {
+            int currentDist=pq.top().first,current=pq.top().second;
             pq.pop();
-            if (current == end) return currentDist;
-            if (currentDist > distances[current]) continue;
-            for (auto& [neighbor, weight]:graph[current]) {
-                int newDist = currentDist + weight;
-                if (newDist < distances[neighbor]) {
-                    distances[neighbor] = newDist;
-                    pq.push({newDist, neighbor});
+            if(current==end) return currentDist;
+            if(currentDist>distances[current]) continue;
+            for(auto& entry:g[current]) {
+                int neighbour=entry.first,weight=entry.second;
+                int newDist=currentDist+weight;
+                if(newDist<distances[neighbour]) {
+                    distances[neighbour]=newDist;
+                    pq.push({newDist,neighbour});
                 }
             }
         }
